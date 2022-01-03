0377356f00b8a6fa567e0401f08b46e651e85ccf
valikhan03 <valikhan.eltaev@gmail.com>
2022-01-03 20:47:57 +0600

    chatlist client add

---
+++ client/templates/chatslist/static/js/chatlist.js
@@ -0,0 +1,13 @@
+const url = "http://localhost:8090/api/my-chats"
+
+fetch(url, {method: "GET"})
+.then((response)=>{
+    response.json()
+    .then((obj)=>{
+        for(var i=0; i<obj.length; i++)
+            document.getElementById("chats-list").innerHTML += "<a href='" + url + "/" + obj[i]["id"] + "'>" + obj[i]["title"] + "</a> <br/>"
+    })
+})
+.catch((err)=>{
+    console.error(err)
+})
